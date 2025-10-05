b0f8afdd5f4308bcc87fe3bb8f406dae0de36fe2
Kshitij Bhusal <kshitijbhusal64@gmail.com>
2025-10-05 10:46:25 +0545

    add function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("hellow from steezcodes")
+    println!("hellow from steezcodes");
+    add(5,6);
+}
+
+
+
+fn add (a:i64, b:i64){
+    let result = a + b;
+
+    println!("the sum is {}", result);
 }
